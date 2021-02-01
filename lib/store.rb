7883e533c93ca577c10cd539bc3efda1bf995479
CD-Mackay <connor.mackay@gmail.com>
2021-02-01 22:44:50 +0000

    implement custom validator, confirm store sells clothing

--- lib/store.rb
+++ lib/store.rb
@@ -1,5 +1,11 @@
 class Store < ActiveRecord::Base
   validates :name, length: {minimum: 3}
   validates :annual_revenue, numericality: { greater_than: 0 }
+  validate :carries_mens_or_womens_clothing
+  def carries_mens_or_womens_clothing
+    if !mens_apparel && !womens_apparel
+      errors.add(:mens_apparel, "Must carry at least one kind of apparel")
+    end
+  end
   has_many :employees
 end
