ae9639c25bd0c12751897290fd0692ed832e5ead
CD-Mackay <connor.mackay@gmail.com>
2021-02-01 21:26:32 +0000

    implement revenue sum, average functions

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "The sum of all revenues... #{Store.sum('annual_revenue')}"
+
+puts "The average of all revenue...#{Store.average('annual_revenue')}"
+
+@large_stores = Store.where("annual_revenue >= ?", [1000000])
+
+puts "The number of stores with an annual revenue greater than $1 million iiis #{@large_stores.count}"
+
